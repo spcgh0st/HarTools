75f85d3e4e4315f9305d16268c138b1022b07fa8
Ernst de Haan <ernst.dehaan@mindcurv.com>
2018-08-28 14:21:51 +0200

    Added support for -t to specify locale for timing numbers.

--- src/ar/com/yamamoto/hartools/har2csv.java
+++ src/ar/com/yamamoto/hartools/har2csv.java
@@ -16,11 +16,12 @@ public class har2csv {
 		argsTrans.put('e', "encoding");
 		argsTrans.put('h', "help");
 		argsTrans.put('i', "in");
-		argsTrans.put('o', "out");		
+		argsTrans.put('o', "out");
+		argsTrans.put('t', "timing-locale");
 		argsTrans.put('v', "version");
-				
+
 		CliArgs cliArgs = new CliArgs(args, argsTrans);
-		
+
 		if(cliArgs.hasArg("help")) {
 			System.out.println("Usage: har2csv options\n"
 				+ "where options include:\n"
@@ -32,30 +33,36 @@ public class har2csv {
 				+ "    -i, --in <filename>       Source HAR filename\n"
 				+ "    -o, --out <filename>      Destination CSV filename\n"
 				+ "                              (if omitted, outputs to stdout)\n"
+				+ "    -t, --timing-locale <l>   Locale to use for the timing output.\n"
 				+ "    -v, --version             Version number\n");
 		} else if(cliArgs.hasArg("version")) {
-			System.out.println("har2csv $build:20140311_2332$\n"
+			System.out.println("har2csv $build:20180828_1419$\n"
 				+ "HTTP Archive(har) to CSV converter\n"
 				+ "http://www.yamamoto.com.ar/hartools\n");
 		} else if(cliArgs.hasArg("in") && String.class.isInstance(cliArgs.getValue("in"))) {
 			HarFile harFile;
-			
+
+			// Get encoding from arguments, if exists
+			String inputEncoding = (String) cliArgs.getValue("encoding");
+			if (inputEncoding == null) {
+				inputEncoding = "UTF-8";
+			}
+
+			// Get output timing locale
+			String timingLocale = (String) cliArgs.getValue("timing-locale");
+
 			try {
-				// Get encoding from arguments, if exists, and call the appropiate constructor				
-				if(cliArgs.hasArg("encoding") && String.class.isInstance(cliArgs.getValue("encoding"))) {
-					harFile = new HarFile((String)cliArgs.getValue("in"), (String)cliArgs.getValue("encoding"));
-				} else {
-					harFile = new HarFile((String)cliArgs.getValue("in"));
-				}
-				
+				// Call the appropriate constructor
+				harFile = new HarFile((String)cliArgs.getValue("in"), inputEncoding, timingLocale);
+
 				// Get delimiter from arguments, if exists
 				if(cliArgs.hasArg("delimiter") && String.class.isInstance(cliArgs.getValue("delimiter"))) {
 					harFile.setDelimiter(((String)cliArgs.getValue("delimiter")).charAt(0));
 				}
-				
+
 				// The actual conversion
 				String csvString = harFile.entriesToCsv();
-				
+
 				// Get destination filename and save content, or print to stdout
 				if(cliArgs.hasArg("out") && String.class.isInstance(cliArgs.getValue("out"))) {
 					TextFile csvFile = new TextFile((String)cliArgs.getValue("out"), "utf-8", true);
@@ -68,7 +75,7 @@ public class har2csv {
 					+ (String)cliArgs.getValue("in") + "\")\n");
 			} catch(UnsupportedCharsetException Ex) {
 				System.out.println("[ERROR] Unknown encoding (\""
-					+ (String)cliArgs.getValue("encoding") + "\")\n");
+					+ (String) inputEncoding + "\")\n");
 			} catch(IOException Ex) {
 				System.out.println("[ERROR] " + Ex.toString() + "\n");
 			} catch(JSONException Ex) {
